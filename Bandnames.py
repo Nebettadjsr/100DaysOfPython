058993ef4881127f504cdf6dd976cab4ed1a6fe1
Bianca <bianca-krause@aikq.de>
2025-01-22 11:45:15 +0100

    Create Bandnames.py
    Final Project Day 1

---
+++ Bandnames.py
@@ -0,0 +1,5 @@
+# Simple print + input functions that combine user answers into a single output sting after storing answers as a value for future reference
+print("Welcome to the Band Name Generator")
+city = input("Where do you live? \n")
+pet = input("What is your Pets Name? \n")
+print("A need Band Name would be: " + city + " " + pet)
